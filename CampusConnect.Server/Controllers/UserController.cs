1d845f9b7574560569aa58a572dfed77a17acce2
Samy <samy.elbetar@yahoo.de>
2025-01-25 01:11:38 +0100

    Changed UserController class Name

--- CampusConnect.Server/Controllers/UserController.cs
+++ CampusConnect.Server/Controllers/UserController.cs
@@ -1,11 +1,17 @@
+using CampusConnect.Server.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CampusConnect.Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class UserController : ControllerBase
     {
+        private readonly CampusConnectContext _context;
 
+        public UserController(CampusConnectContext context)
+        {
+            _context = context;
+        }
     }
 }
