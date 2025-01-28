e5a9029388e5fa36eb3523fb05e1eca8e703774e
Samy Elbetar <samy.elbetar@yahoo.de>
2025-01-28 23:48:54 +0100

    Provisorisches Login und Register UI angelegt

--- campusconnect.client/src/app/app.module.ts
+++ campusconnect.client/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
