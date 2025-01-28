1021b04abd858a39edf66ebebe9edeedd2600608
Samy <samy.elbetar@yahoo.de>
2025-01-28 22:21:27 +0100

    grundlegende Api Requests im UserController angelegt

--- CampusConnect.Server/Controllers/UserController.cs
+++ CampusConnect.Server/Controllers/UserController.cs
@@ -1,5 +1,10 @@
 using CampusConnect.Server.Data;
+using CampusConnect.Server.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CampusConnect.Server.Controllers
 {
@@ -12,6 +17,44 @@ namespace CampusConnect.Server.Controllers
         public UserController(CampusConnectContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<ActionResult<User>> GetUserById(int userId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+            return user is not null ? Ok(user) : NotFound();
+        }
+
+        //TODO: Login Request
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<User>>> PostNewUser(User user)
+        {
+            if (user is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserById", new { id = user.UserId }, user);
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> DeleteUser(int userId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+
+            if (user is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
