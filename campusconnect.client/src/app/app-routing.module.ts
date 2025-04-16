5e9beacb6bae8290fd0cd79cf36da93b4ef8aefd
Leon Trumann <leon.trumann@st.ovgu.de>
2025-04-16 17:37:13 +0200

    updated: Routing hinzugefügt und Darkmode als BG eingestellt

--- campusconnect.client/src/app/app-routing.module.ts
+++ campusconnect.client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { OnboardingComponent } from './onboarding/onboarding.component';
 
 export const baseApiRoute = 'https://localhost:7109/';
 
@@ -14,6 +15,10 @@ const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent
+    },
+    {
+        path: 'onboarding',
+        component: OnboardingComponent
     },
     {
         path: '',
