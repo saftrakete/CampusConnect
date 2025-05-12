0554ecbbea1fa8ebdd8c3309d3a6ca7880ee2479
anton <anton.dudicz@st.ovgu.de>
2025-05-12 12:38:20 +0200

    created chatComponent

---
+++ campusconnect.client/src/app/chat/chat.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat.service';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrl: './chat.component.scss'
+})
+export class ChatComponent implements OnInit {
+  constructor(private chatService: ChatService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  public chatForm!: FormGroup;
+
+  private readonly emptyString: string = '';
+
+  public ngOnInit(): void {
+      this.chatForm = this.formBuilder.group({
+         chatMessage: [this.emptyString, Validators.required]
+      }
+    );
+  }
+
+  public chatClick(): void {
+      if (!this.chatForm.valid.valueOf()) {
+          console.log('unvalid form');
+          return;
+      }
+
+      let chatEntity = this.chatService.createMessageEntity(
+          this.chatForm.get('chatMessage')?.value,
+      );
+      
+      this.chatService.postNewMessage(chatEntity).subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+ 
+        }
+      );
+  }
+}
