0b863c9068ed28ceb5311e4a8e3ab9e511df348e
Leon <leon.trumann@st.ovgu.de>
2025-08-07 17:22:08 +0200

    Confirm Button hinzugefügt --> Funktion muss noch vollendet werden

--- campusconnect.client/src/app/dashboard/dashboard.component.ts
+++ campusconnect.client/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OnboardingService } from '../services/onboarding.service';
+import { AuthorizationService } from '../services/authorization.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +12,7 @@ export class DashboardComponent implements OnInit {
 
   filterQuery: string = '';
 
-  constructor(public os: OnboardingService) {}
+  constructor(public os: OnboardingService, public auth: AuthorizationService, public us: UserService) {}
 
     ngOnInit(): void {
     this.os.loadOnboardingDataFromLocalStorage();
@@ -25,6 +27,22 @@ export class DashboardComponent implements OnInit {
   public resetFiltering() {
     this.filterQuery = '';
     this.os.StartOnboardingFiltering();
+  }
+
+  public confirmActions() {
+    const token = localStorage.getItem("jwt");
+    const loginName = this.auth.getLoginName();
+    if (loginName != null) {
+      this.us.getUserIdByLoginName(loginName).subscribe(
+        (userId) => {
+          console.log(userId);
+          // Id nutzen um addedModules als Liste in der User Tabelle für den individuellen Nutzer abzuspeichern
+          //
+          // TODO: UserModel anpassen, dass Listen gespeichert werden können. Anfrage ans Backend senden und Liste hinterlegen.
+        }
+      )
+    }
+
   }
 
 }
