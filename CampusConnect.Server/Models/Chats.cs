c56676b61f90ac1c67f62fb3536dd93b422c36c2
Felix-Hill <fhillma@web.de>
2025-08-20 16:51:07 +0200

    #85 [added] alle geforderten tabellen hinzugefüggt wie  im issue, Primaraykey für Chats als ChatId festgelegt

---
+++ CampusConnect.Server/Models/Chats.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CampusConnect.Server.Models
+{
+    [PrimaryKey("ChatId")]
+    public class Chats
+    {
+        public int ChatId { get; set; }
+        public string ChatName { get; set; }
+        public string ChatAdmin { get; set; }
+    }
+}
