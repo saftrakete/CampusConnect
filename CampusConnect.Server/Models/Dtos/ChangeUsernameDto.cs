73f9645889bcaadad5107162603dbf1938cbce26
Leon <leon.trumann@st.ovgu.de>
2025-08-21 14:44:41 +0200

    #77 [ADDED] ChangeUsername Dto für Backend erstellt

---
+++ CampusConnect.Server/Models/Dtos/ChangeUsernameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CampusConnect.Server.Models.Dtos
+{
+    public class ChangeUsernameDto
+    {
+        public string NewNickname { get; set; }
+        public string LoginName { get; set; }
+    }
+}
